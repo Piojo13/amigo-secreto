c55869f55dc706b1c828ea2068d863950a81ab29
Rodrigo <cesar.dominguez.5021@gmail.com>
2025-08-10 20:20:25 -0300

    Actualizacion agregar amigo

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
 let nombresAmigos = []
 
 function agregarAmigo() {
-    var nombre = document.getElementById("amigo")
-    nombresAmigos.push(nombre)
+    var nombre = document.getElementById("amigo").value.trim(); //Extrae el nombre añadido
+    //Si el nombre no esta repetido, lo añade. Caso contrario, pide ingresar una distincion.
+    if (nombre !== "") {
+        if (nombresAmigos.includes(nombre)) {
+        alert("Ese nombre ya fue incluido en la lista, ingrese otro o utilice una distinción.");
+       }  else { 
+             nombresAmigos.push(nombre);
+            document.getElementById("amigo").value = "";
+       }
+    } else {
+        alert("Por favor, ingrese un nombre.")
+    }  
 }
 function sortearAmigo() {
 
