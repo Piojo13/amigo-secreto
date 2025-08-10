36548b24fc15e73822724ee3824e9e76cccf591c
Rodrigo <cesar.dominguez.5021@gmail.com>
2025-08-10 19:43:41 -0300

    Creacion del proyecto

---
+++ app.js
@@ -0,0 +1,11 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombresAmigos = []
+
+function agregarAmigo() {
+    var nombre = document.getElementById("amigo")
+    nombresAmigos.push(nombre)
+}
+function sortearAmigo() {
+
+}
