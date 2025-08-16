8fcc0d6731307e2982928d6b85e56b958cf4ee71
Rodrigo <cesar.dominguez.5021@gmail.com>
2025-08-15 23:53:38 -0300

    Actualizacion app.js
    agregarAmigo funciona ; mostrarLista no funciona; sortearAmigo funciona

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let nombresAmigos = []
+let nombresAmigos = [];
 
 function agregarAmigo() {
-    var nombre = document.getElementById("amigo").value.trim(); //Extrae el nombre añadido
-    //Si el nombre no esta repetido, lo añade. Caso contrario, pide ingresar una distincion.
+    let nombre = document.getElementById("amigo").value.trim();
     if (nombre !== "") {
         if (nombresAmigos.includes(nombre)) {
         alert("Ese nombre ya fue incluido en la lista, ingrese otro o utilice una distinción.");
@@ -13,18 +12,27 @@ function agregarAmigo() {
             document.getElementById("amigo").value = "";
        }
     } else {
-        alert("Por favor, ingrese un nombre.")
+        alert("Por favor, ingrese un nombre.");
     }  
-}
+};
 function mostrarLista() {
-    //Obtener nombre
-    //document.getElementById("listaAmigos")
-    //
-}
-function sortearAmigo() {
-    if (nombresAmigos = "") {
-        alert("La lista de amigos esta vacia, no es posible sortear")
-    } else {
-        let numeroLista = Math.floor(Math.random())
+    let listaNombres = document.getElementById("listaAmigos");
+    for (const amigo of nombresAmigos) {
+     listaNombres.innerHTML += `<li>${amigo}</li>`;
     }
-}
+    //listaNombres.innerHTML = "";
+};
+
+function sortearAmigo() {
+    if (nombresAmigos == "") {
+        alert("La lista de amigos esta vacia, no es posible sortear");
+        return;
+    } else { if (nombresAmigos.length < 2) {
+        alert("Por favor, ingrese como minimo 2 nombres para sortear") 
+         }else {
+            let numeroLista = Math.floor(Math.random()* nombresAmigos.length);
+            let nombreAleatorio = nombresAmigos[numeroLista];
+            document.getElementById("resultado").innerHTML = `Tu amigo secreto es ${nombreAleatorio}`;
+         }
+    }
+};
